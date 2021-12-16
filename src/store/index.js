4501bbe4dd565ac4b649856311d71ad123a54657
david1707 <DavidAlacant@gmail.com>
2021-12-16 11:46:52 +0100

    Created Signup form, Vuex and View functions, and added Firebase config file

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,24 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
+
+import { auth } from "@/firebase/config";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
 export default createStore({
   state: {
-  },
-  mutations: {
+    user: null,
   },
   actions: {
+    async signup(context, { email, password }) {
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+      console.log(res);
+      if (res) context.commit("updateUser", res.user);
+      else throw new Error("Error while registering a new user.");
+    },
   },
-  modules: {
-  }
-})
+  mutations: {
+    updateUser(state, payload) {
+      state.user = payload;
+    },
+  },
+  modules: {},
+});
--- src/views/auth/Signup.vue
+++ src/views/auth/Signup.vue
@@ -1,13 +1,40 @@
 <template>
   <h2>Signup</h2>
+  <form @submit.prevent="submitSignup">
+    <label for="email">Email:</label>
+    <input name="email" type="email" v-model="email" required />
+
+    <label for="password">Password:</label>
+    <input name="password" type="password" v-model="password" required />
+
+    <button>Sign up</button>
+    <div v-if="error">{{ error }}</div>
+  </form>
 </template>
 
 <script>
-export default {
+import { ref } from "@vue/reactivity";
+import { useStore } from "vuex";
 
-}
+export default {
+  setup() {
+    const email = ref("");
+    const password = ref("");
+    const error = ref(null);
+
+    const store = useStore();
+
+    const submitSignup = async () => {
+      await store.dispatch("signup", {
+        email: email.value,
+        password: password.value,
+      });
+    };
+
+    return { submitSignup, email, password, error };
+  },
+};
 </script>
 
 <style>
-
 </style>
